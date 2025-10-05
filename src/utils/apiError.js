ccceca63ac513caaa7d983c1adef888824834e2a
ayushverse <56singh5055@gmail.com>
2025-10-05 18:53:19 +0600

    addded user and video model

---
+++ src/utils/apiError.js
@@ -0,0 +1,17 @@
+class apiError extends Error {
+    constructor(statusCode, message = "Something went wrong", error = [], stack = "") {
+        super(message);
+        this.statusCode = statusCode;
+        this.message = message;
+        this.status = statusCode;
+        this.error = error;
+        this.success = false;
+
+        if (stack) {
+            this.stack = stack;
+        } else {
+            Error.captureStackTrace(this, this.constructor);
+        }
+    }
+}
+export { apiError };
